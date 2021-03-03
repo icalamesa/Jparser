9b05ab949acacaab4a6d05b5c8abbbc8f0fab016
icalamesa <icalamesa@gmail.com>
2021-03-03 23:57:52 +0100

    first working (only string parsing) version.

--- source/bin/main.cpp
+++ source/bin/main.cpp
@@ -3,8 +3,8 @@
 int main()
 {
     //algo
-    jlib::jmap thing(std::string("smth"));
-    thing.out(std::string("./"), "hola");
-    thing.add_entry(std::string("mylove"), std::string("shutup"));
-
+    jlib::jmap thing(std::string("../../data/test.json"));
+    thing.add_entry(std::string("pene"), std::string("shutup"));
+    std::cout << thing.get_str(std::string("hello")) << std::endl;
+    std::cout << thing.get_str(std::string("hello2")) << std::endl;
 }
--- source/parser/parser.cpp
+++ source/parser/parser.cpp
@@ -33,7 +33,7 @@
 
     jmap::jmap(std::string name) 
     {
-        std::ifstream file("../../data/test.json", std::ios_base::in);
+        std::ifstream file(name.data(), std::ios_base::in);
         std::string data_stream((std::istreambuf_iterator<char>(file)),(std::istreambuf_iterator<char>() )   );
         parse(data_stream);
     }
@@ -50,7 +50,6 @@
         status the_status = status::EXPECT_OPEN;
         for (size_t a = 0; a < in.size(); a++)
         {
-            std::cout << in[a];
             if (the_status == status::EXPECT_OPEN)
             {
                 if (in[a] <= ' ') continue;
@@ -58,23 +57,28 @@
                 {
                     the_status = status::EXPECT_KEY;
                     //stack increase
+                    continue;
                 }
                 else throw std::runtime_error("No bracket opening added to input json!");
             }
             
-            //if we expect to find a keyword
-            if (the_status == status::EXPECT_KEY)
+            else if (the_status == status::EXPECT_KEY)
             {
+
+                //if we expect to find a keyword
                 if (in[a] <= ' ') continue;
                 else if (in[a] <= '\"')
                 {
                     the_status = status::OPEN_KEY;
+                    continue;
                 }
+                continue;
             }
             
-            //if already reading key
-            if (the_status == status::OPEN_KEY)
+            else if (the_status == status::OPEN_KEY)
             {
+
+                //if already reading key
                 //if " are detected and are not part of the string itself
                 if (in[a] == '\"' and in[a-1] != '\\')
                 {
@@ -86,47 +90,52 @@
                     
                     key_aux.push_back(in[a]);
                 }
+                continue;
             }
 
-            if (the_status == status::EXPECT_COLON)
+            else if (the_status == status::EXPECT_COLON)
             {
                 if (in[a] == ':')
                 {
                     the_status = status::EXPECT_VAL;
                     continue;
                 }
-                else if (in[a] <= ' ') continue;
                 //else throw std::runtime_error("No semicolon found bruh");
+                continue;
             }
 
-            if (the_status == status::EXPECT_VAL)
+            else if (the_status == status::EXPECT_VAL)
             {
                 if (in[a] <= ' ') continue;
                 else if (in[a] == '\"')
                 {
                     the_status = status::OPEN_VAL;
-                    continue;
                 }
                 else throw std::runtime_error("no val found");
+                continue;
             }
 
-            if (the_status == status::OPEN_VAL)
+            else if (the_status == status::OPEN_VAL)
             {
-                if (in[a] <= ' ') continue;
                 if (in[a] == '\"' and in[a-1] != '\\')
                 {
-                    value_aux.push_back(in[a]);
                     the_status = status::EXPECT_ACTION;
                     add_entry(key_aux, value_aux);
-                    continue;
                 }
+                else value_aux.push_back(in[a]);
+                continue;
             }
 
-            if (the_status == status::EXPECT_ACTION)
+            else if (the_status == status::EXPECT_ACTION)
             {
-                if (in[a] <= ' ') continue;
-                else if (in[a] == '}') break;
-                else if (in[a] == ',') the_status = status::EXPECT_KEY;
+                if (in[a] == '}') break;
+                else if (in[a] == ',') 
+                {
+                    the_status = status::EXPECT_KEY;
+                    key_aux.clear();
+                    value_aux.clear();
+                    continue;
+                }
                 else throw std::runtime_error("no end bracket found");
             }
 
