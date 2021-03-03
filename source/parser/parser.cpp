7cfeab78413cf3711e62e622d2ef9fc196b0a5f5
icalamesa <icalamesa@gmail.com>
2021-03-03 18:01:42 +0100

    .

--- source/parser/parser.cpp
+++ source/parser/parser.cpp
@@ -44,9 +44,10 @@
                     the_status = status::EXPECT_KEY;
                     //stack increase
                 }
-                else throw "No bracket opening added to input json!"
+                else throw "No bracket opening added to input json!";
             }
-
+            
+            //if we expect to find a keyword
             if (the_status == status::EXPECT_KEY)
             {
                 if (in[a] <= ' ') continue;
@@ -55,9 +56,11 @@
                     the_status = status::OPEN_KEY;
                 }
             }
-
+            
+            //if already reading key
             if (the_status == status::OPEN_KEY)
             {
+                //if " are detected and are not part of the string itself
                 if (in[a] == '\"' and in[a-1] != '\\')
                 {
                     the_status = status::EXPECT_COLON;
@@ -68,6 +71,46 @@
                     
                     key_aux.push_back(in[a]);
                 }
+            }
+
+            if (the_status == status::EXPECT_COLON)
+            {
+                if (in[a] == ':')
+                {
+                    the_status = status::EXPECT_VAL;
+                    continue;
+                }
+                else if (in[a] <= ' ') continue;
+                else throw "No semicolon found bruh";
+            }
+
+            if (the_status == status::EXPECT_VAL)
+            {
+                if (in[a] <= ' ') continue;
+                else if (in[a] == '\"')
+                {
+                    the_status = status::OPEN_VAL;
+                    continue;
+                }
+                else throw "no val found";
+            }
+
+            if (the_status == status::OPEN_VAL)
+            {
+                if (in[a] <= ' ') continue;
+                if (in[a] == '\"' and in[a-1] != '\\')
+                {
+                    value_aux.push_back(in[a]);
+                    the_status = status::EXPECT_ACTION;
+                    continue;
+                }
+            }
+
+            if (the_status == status::EXPECT_ACTION)
+            {
+                if (in[a] <= ' ') continue;
+                if (in[a] == '}') break;
+                else throw "no end bracket found";
             }
         }
     }
